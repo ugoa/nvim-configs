509a66e7934e36c4565218248c6d8379e4843d83
Dawei Hu <dawei.hu@gxs.com.sg>
2023-12-12 00:56:29 +0800

    chore: [main] show git blame

--- plugins.lua
+++ plugins.lua
@@ -6,6 +6,20 @@ local plugins = {
   },
 
   {
+    "lewis6991/gitsigns.nvim",
+    opts = {
+      current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+      current_line_blame_opts = {
+        virt_text = true,
+        virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+        delay = 0,
+        ignore_whitespace = false,
+        virt_text_priority = 100,
+      },
+    },
+  },
+
+  {
     "NvChad/nvim-colorizer.lua",
     enabled = false,
     opts = {
