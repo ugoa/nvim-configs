1337adb4500138dc4dc354650a760f8834f22a10
Dawei Hu <dawei.hu@gxs.com.sg>
2023-12-14 10:44:15 +0800

    chore: [main] add auto save plugin

--- plugins.lua
+++ plugins.lua
@@ -6,6 +6,15 @@ local plugins = {
   },
 
   {
+    "Pocco81/auto-save.nvim",
+    lazy = false,
+    opts = {
+      trigger_events = {"InsertLeave", "TextChanged", "FocusLost"},
+      write_all_buffers = true,
+    }
+  },
+
+  {
     "lewis6991/gitsigns.nvim",
     opts = {
       current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
