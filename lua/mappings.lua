c344c78fa607c9676e1335a7b8a53d0f1d12c339
Dawei Hu <dawei.hu@gxs.com.sg>
2024-03-25 21:13:06 +0800

    chore: [main] add vim-tmux support

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -3,16 +3,13 @@ require("nvchad.mappings")
 -- add yours here
 
 local map = vim.keymap.set
-
 map("i", "<C-s>", "<C-o>:w<CR>", { desc = "Save file" })
 
 map("i", "<C-f>", "<Right>", { desc = "Move right" })
 map("i", "<C-b>", "<Left>", { desc = "Move left" })
 map("i", "<C-a>", "<Home>", { desc = "Move beginning" })
 map("i", "<C-e>", "<End>", { desc = "Move to end" })
-
 map("i", "<F1>", "<Nop>", { desc = "Disable" })
-
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("n", "<leader>op", ":!open %:p:h<CR>", { desc =  "Open in Finder"})
 
@@ -29,3 +26,17 @@ map("c", "<C-f>", "<Right>", { desc = "Move right" })
 map("c", "<C-b>", "<Left>", { desc = "Move left" })
 map("c", "<C-a>", "<Home>", { desc = "Move beginning" })
 map("c", "<C-e>", "<End>", { desc = "Move to end" })
+
+-- Disable mappings
+local nomap = vim.keymap.del
+nomap("n", "<C-h>")
+nomap("n", "<C-j>")
+nomap("n", "<C-k>")
+nomap("n", "<C-l>")
+
+map('n', "<M-h>", "<Cmd>NvimTmuxNavigateLeft<CR>")
+map('n', "<M-j>", "<Cmd>NvimTmuxNavigateDown<CR>")
+map('n', "<M-k>", "<Cmd>NvimTmuxNavigateUp<CR>")
+map('n', "<M-l>", "<Cmd>NvimTmuxNavigateRight<CR>")
+map('n', "<M-\\>", "<Cmd>NvimTmuxNavigateLastActive<CR>")
+
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,6 +1,14 @@
 return {
-
-  { "alexghergh/nvim-tmux-navigation" },
+    {
+      'alexghergh/nvim-tmux-navigation',
+      lazy = false,
+      config = function()
+        local p = require('nvim-tmux-navigation')
+        p.setup {
+            disable_when_zoomed = true -- defaults to false
+        }
+    end
+  },
 
 	{
 		"stevearc/conform.nvim",
