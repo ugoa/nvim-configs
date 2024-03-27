f63ce7047f10123c0e14176d672bfe752e2d0fdc
Dawei Hu <dawei.hu@gxs.com.sg>
2024-03-27 18:18:23 +0800

    chore: [main] format python

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -5,6 +5,7 @@ return {
 		local options = {
 			formatters_by_ft = {
 				lua = { "stylua" },
+				python = { "black" },
 			},
 			format_on_save = { -- These options will be passed to conform.format()
 				timeout_ms = 500,
