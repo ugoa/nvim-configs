0ca3eb31328823b2053950ecf5afe96204dbe125
Dawei Hu <dawei.hu@gxs.com.sg>
2024-03-27 13:32:27 +0800

    chore: [main] enable confirm

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,11 +1,14 @@
 return {
 	"stevearc/conform.nvim",
+	event = "BufWritePre",
 	config = function()
 		local options = {
-			lsp_fallback = true,
-
 			formatters_by_ft = {
 				lua = { "stylua" },
+			},
+			format_on_save = { -- These options will be passed to conform.format()
+				timeout_ms = 500,
+				lsp_fallback = true,
 			},
 		}
 		require("conform").setup(options)
