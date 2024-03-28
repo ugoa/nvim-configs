9acfbbcc7f2363eb650eef4260bdbec21cc881fe
Dawei Hu <hoodavy@gmail.com>
2024-03-28 21:11:56 +0800

    show file path on statusline

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -6,6 +6,18 @@ M.ui = {
 
 	statusline = {
 		theme = "vscode_colored",
+		order = { "mode", "myfile", "diagnostics", "git", "%=", "lsp_msg", "%=", "lsp", "cursor", "cwd" },
+		modules = {
+			myfile = function()
+				local path = vim.fn.expand("%")
+				local buf = vim.api.nvim_win_get_buf(vim.g.statusline_winid or 0)
+				local path1 = vim.api.nvim_buf_get_name(buf)
+				local utils = require("nvchad.stl.utils")
+				local x = utils.file()
+				local name = " " .. path .. " "
+				return "%#StText# " .. x[1] .. name
+			end,
+		},
 	},
 	term = {
 		float = {
