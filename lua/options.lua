1cb9090a6a45366e476a029127a55d1c8b8673e0
Dawei Hu <hoodavy@gmail.com>
2024-03-29 16:44:06 +0800

    add comments

--- lua/options.lua
+++ lua/options.lua
@@ -3,4 +3,6 @@ require("nvchad.options")
 -- add yours here!
 
 -- FIX: https://github.com/NvChad/NvChad/issues/1907
+-- Another solution is to disable sematic token
+-- https://github.com/NvChad/NvChad/blob/v2.5/lua/nvchad/configs/lspconfig.lua#L41
 vim.highlight.priorities.semantic_tokens = 95 -- Or any number lower than 100, treesitter's priority level
