659a1b9f979d34c29a9b8f5ee43893a22cd66d19
Dawei Hu <hoodavy@gmail.com>
2024-03-30 01:43:32 +0800

    fix lsp treesitter highlighting conflict

--- lua/options.lua
+++ lua/options.lua
@@ -5,4 +5,4 @@ require("nvchad.options")
 -- FIX: https://github.com/NvChad/NvChad/issues/1907
 -- Another solution is to disable sematic token in rust-tools on_init function
 -- https://github.com/NvChad/NvChad/blob/v2.5/lua/nvchad/configs/lspconfig.lua#L41
--- vim.highlight.priorities.semantic_tokens = 95 -- Or any number lower than 100, treesitter's priority level
+vim.highlight.priorities.semantic_tokens = 95 -- Or any number lower than 100, treesitter's priority level
