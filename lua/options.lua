ff94b900b6c05ac736d35f49f12eec3b278bc58e
Dawei Hu <hoodavy@gmail.com>
2024-03-29 16:28:18 +0800

    fix colortheme

--- lua/options.lua
+++ lua/options.lua
@@ -1,3 +1,6 @@
 require("nvchad.options")
 
 -- add yours here!
+
+-- FIX: https://github.com/NvChad/NvChad/issues/1907
+vim.highlight.priorities.semantic_tokens = 95 -- Or any number lower than 100, treesitter's priority level
