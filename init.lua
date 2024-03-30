d69a7fa5a6188cb3c432a5c663fca05bec6e62e7
Dawei Hu <hoodavy@gmail.com>
2024-03-31 01:30:55 +0800

    reduce init

--- init.lua
+++ init.lua
@@ -5,55 +5,6 @@ vim.g.mapleader = " "
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 vim.opt.rtp:prepend(lazypath)
 
-local lazy_config = {
-	defaults = { lazy = true },
-	install = { colorscheme = { "nvchad" } },
-
-	ui = {
-		icons = {
-			ft = "",
-			lazy = "󰂠 ",
-			loaded = "",
-			not_loaded = "",
-		},
-	},
-
-	performance = {
-		rtp = {
-			disabled_plugins = {
-				"2html_plugin",
-				"tohtml",
-				"getscript",
-				"getscriptPlugin",
-				"gzip",
-				"logipat",
-				"netrw",
-				"netrwPlugin",
-				"netrwSettings",
-				"netrwFileHandlers",
-				"matchit",
-				"tar",
-				"tarPlugin",
-				"rrhelper",
-				"spellfile_plugin",
-				"vimball",
-				"vimballPlugin",
-				"zip",
-				"zipPlugin",
-				"tutor",
-				"rplugin",
-				"syntax",
-				"synmenu",
-				"optwin",
-				"compiler",
-				"bugreport",
-				"ftplugin",
-			},
-		},
-	},
-}
-
--- load plugins
 require("lazy").setup({
 	{
 		"NvChad/NvChad",
@@ -66,7 +17,7 @@ require("lazy").setup({
 	},
 
 	{ import = "plugins" },
-}, lazy_config)
+}, require("configs.lazy"))
 
 -- load theme
 dofile(vim.g.base46_cache .. "defaults")
