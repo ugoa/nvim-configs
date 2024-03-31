2f43323f0e45dddcb5dd7a27b4529f6d075b8697
Dawei Hu <dawei.hu@gxs.com.sg>
2024-03-31 10:06:22 +0800

    chore: [main] fix autosession

--- lua/plugins/auto-session.lua
+++ lua/plugins/auto-session.lua
@@ -11,8 +11,6 @@ return {
 	config = function()
 		require("auto-session").setup({
 			log_level = "error",
-			auto_session_suppress_dirs = { "~/", "~/Downloads", "/" },
-			auto_session_root_dir = "~/.local/state/nvim/sessions",
 			post_restore_cmds = { openTree },
 		})
 	end,
