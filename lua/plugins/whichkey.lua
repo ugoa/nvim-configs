998cd033dfcce7547b1a831a8c8fddbf36fbf762
Dawei Hu <hoodavy@gmail.com>
2024-04-02 20:52:04 +0800

    fix indent

--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -1,12 +1,10 @@
 return {
-	{
-		"folke/which-key.nvim",
-		lazy = false,
-		config = function(_, opts)
-			dofile(vim.g.base46_cache .. "whichkey")
-			vim.o.timeout = true
-			vim.o.timeoutlen = 0
-			require("which-key").setup(opts)
-		end,
-	},
+	"folke/which-key.nvim",
+	lazy = false,
+	config = function(_, opts)
+		dofile(vim.g.base46_cache .. "whichkey")
+		vim.o.timeout = true
+		vim.o.timeoutlen = 0
+		require("which-key").setup(opts)
+	end,
 }
