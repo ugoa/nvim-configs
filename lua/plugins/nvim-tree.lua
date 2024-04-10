95e1c5a31d08a2b9d08bf15d98c2e5c5303595a6
Dawei Hu <hoodavy@gmail.com>
2024-04-10 13:39:55 +0800

    disable root dir change

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,9 +1,48 @@
+local function open_or_expand()
+	local lib = require("nvim-tree.lib")
+	local mode = "edit"
+	local toggle_group = false
+	local node = lib.get_node_at_cursor()
+	if node.name == ".." then
+		return
+	elseif node.nodes then
+		lib.expand_or_collapse(node, toggle_group)
+	elseif not toggle_group then
+		local path = node.absolute_path
+		if node.link_to and not node.nodes then
+			path = node.link_to
+		end
+		require("nvim-tree.actions").node.open_file.fn(mode, path)
+	end
+end
+
+local function my_on_attach(bufnr)
+	local api = require("nvim-tree.api")
+
+	api.node.open.edit = open_or_expand
+
+	local function opts(desc)
+		return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+	end
+	-- default mappings
+	api.config.mappings.default_on_attach(bufnr)
+
+	-- custom mappings
+	vim.keymap.set("n", "<C-]>", "<nop>", opts("CD"))
+end
+
 return {
 	"nvim-tree/nvim-tree.lua",
 	opts = {
+		on_attach = my_on_attach,
 		git = { enable = true },
 		filters = {
 			dotfiles = true,
+		},
+		actions = {
+			change_dir = {
+				enable = false,
+			},
 		},
 		renderer = {
 			root_folder_label = ":~:s?$?",
