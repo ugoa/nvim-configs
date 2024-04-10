7748dacaa8b5d5e8d88106cba0dc436a3e549fdb
Dawei Hu <hoodavy@gmail.com>
2024-04-10 16:15:59 +0800

    more mappings of nvimtree

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -28,9 +28,13 @@ local function my_on_attach(bufnr)
 	api.config.mappings.default_on_attach(bufnr)
 
 	-- custom mappings
-	vim.keymap.set("n", "<C-]>", "<nop>", opts("Disable Root change"))
-	vim.keymap.set("n", "-", "<nop>", opts("Disable Up"))
-	vim.keymap.set("n", "<2-RightMouse>", "<nop>", opts("Disable Root change"))
+	vim.keymap.set("n", "<C-]>", "<nop>", opts("Disabled"))
+	vim.keymap.set("n", "-", "<nop>", opts("Disabled"))
+	vim.keymap.set("n", "<2-RightMouse>", "<nop>", opts("Disabled"))
+	vim.keymap.set("n", "g?", "<nop>", opts("Disabled"))
+	vim.keymap.set("n", "?", api.tree.toggle_help, opts("Help"))
+	vim.keymap.set("n", "f", api.live_filter.clear, opts("Live Filter: Clear"))
+	vim.keymap.set("n", "F", api.live_filter.start, opts("Live Filter: Start"))
 end
 
 return {
