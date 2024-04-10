e3f126fce9c22a86373434f5a77cf029d4637e64
Dawei Hu <hoodavy@gmail.com>
2024-04-10 22:33:33 +0800

    temp disable prettier

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -6,7 +6,6 @@ return {
 			formatters_by_ft = {
 				lua = { "stylua" },
 				python = { "black" },
-				javascript = { "prettier" },
 			},
 			format_on_save = { -- These options will be passed to conform.format()
 				timeout_ms = 500,
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -11,6 +11,7 @@ return {
 			"stylua",
 			-- for vue.js
 			"vue-language-server",
+			-- "prettier",
 		},
 	},
 }
