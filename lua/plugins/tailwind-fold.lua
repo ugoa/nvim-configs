8dd926bdcaefbbc0ee4d7368798f618a036137eb
Dawei Hu <hoodavy@gmail.com>
2024-05-04 20:50:14 +0800

    remove unused plugin

--- lua/plugins/tailwind-fold.lua
+++
@@ -1,7 +0,0 @@
-return {
-	"razak17/tailwind-fold.nvim",
-	enabled = false,
-	opts = {},
-	dependencies = { "nvim-treesitter/nvim-treesitter" },
-	ft = { "html", "svelte", "astro", "javascriptreact", "vue", "typescriptreact" },
-}
