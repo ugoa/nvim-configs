6fba89daf8f32b1421c3d24a4d5227baeb8355ec
Dawei Hu <hoodavy@gmail.com>
2024-10-19 13:35:10 +0800

    set html indent

--- lua/options.lua
+++ lua/options.lua
@@ -7,7 +7,7 @@ vim.opt.colorcolumn = "120"
 -- vim.opt.shell = "/opt/homebrew/bin/nu"
 vim.opt.shell = "/bin/zsh"
 
-vim.o.tabstop = 2 -- A TAB character looks like 4 spaces
+vim.o.tabstop = 2 -- A TAB character looks like n spaces
 vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
 vim.o.softtabstop = 2 -- Number of spaces inserted instead of a TAB character
 vim.o.shiftwidth = 2 -- Number of spaces inserted when indenting
