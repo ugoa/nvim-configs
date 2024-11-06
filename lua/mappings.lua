772ab48b857f8e08b6ae68f8ede3ed564d591670
Dawei Hu <dawei.hu@gxs.com.sg>
2024-11-06 17:50:41 +0800

    chore: [main] remap

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -113,7 +113,5 @@ vim.keymap.set("n", "*", "*zz", { silent = true })
 vim.keymap.set("n", "#", "#zz", { silent = true })
 vim.keymap.set("n", "g*", "g*zz", { silent = true })
 
-vim.keymap.set("n", "<C-f>", "<C-f>zz", { silent = true })
-vim.keymap.set("n", "<C-b>", "<C-b>zz", { silent = true })
 vim.keymap.set("n", "<C-u>", "<C-u>zz", { silent = true })
 vim.keymap.set("n", "<C-d>", "<C-d>zz", { silent = true })
