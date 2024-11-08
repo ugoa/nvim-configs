04fb90112d5b0ad517687f9ae5fab8d2da83055e
Dawei Hu <dawei.hu@gxs.com.sg>
2024-11-08 21:04:01 +0800

    chore: [main] fix build

--- init.lua
+++ init.lua
@@ -1,9 +1,14 @@
-vim.g.base46_cache = vim.fn.stdpath("data") .. "/nvchad/base46/"
+vim.g.base46_cache = vim.fn.stdpath("data") .. "/base46/"
 vim.g.mapleader = " "
 
 -- bootstrap lazy and all plugins
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 vim.opt.rtp:prepend(lazypath)
+
+if not vim.uv.fs_stat(lazypath) then
+	local repo = "https://github.com/folke/lazy.nvim.git"
+	vim.fn.system({ "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath })
+end
 
 local lazyconf = {
 	defaults = { lazy = true },
@@ -68,8 +73,6 @@ require("lazy").setup({
 dofile(vim.g.base46_cache .. "defaults")
 dofile(vim.g.base46_cache .. "statusline")
 
+require("options")
 require("autocmds")
-
-vim.schedule(function()
-	require("mappings")
-end)
+require("mappings")
