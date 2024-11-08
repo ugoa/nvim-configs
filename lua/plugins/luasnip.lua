15b2a360c0d3ea5817497c1f9689f6e3e029dc43
Dawei Hu <dawei.hu@gxs.com.sg>
2024-11-08 18:13:37 +0800

    chore: [main] link html with htmldjango

--- lua/plugins/luasnip.lua
+++
@@ -1,6 +0,0 @@
-return {
-	"L3MON4D3/LuaSnip",
-	config = function()
-		require("luasnip").filetype_extend("htmldjango", { "html" })
-	end,
-}
--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -2,7 +2,48 @@ return {
 	"hrsh7th/nvim-cmp",
 	lazy = false,
 
-	dependencies = { "hrsh7th/cmp-cmdline", "hrsh7th/cmp-path", "hrsh7th/cmp-buffer" },
+	-- dependencies = { "hrsh7th/cmp-cmdline", "hrsh7th/cmp-path", "hrsh7th/cmp-buffer" },
+
+	dependencies = {
+		{
+			-- snippet plugin
+			"L3MON4D3/LuaSnip",
+			dependencies = "rafamadriz/friendly-snippets",
+			opts = { history = true, updateevents = "TextChanged,TextChangedI" },
+			config = function(_, opts)
+				require("luasnip").config.set_config(opts)
+				require("luasnip").filetype_extend("htmldjango", { "html" })
+				require("nvchad.configs.luasnip")
+			end,
+		},
+
+		-- autopairing of (){}[] etc
+		{
+			"windwp/nvim-autopairs",
+			opts = {
+				fast_wrap = {},
+				disable_filetype = { "TelescopePrompt", "vim" },
+			},
+			config = function(_, opts)
+				require("nvim-autopairs").setup(opts)
+
+				-- setup cmp for autopairs
+				local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+				require("cmp").event:on("confirm_done", cmp_autopairs.on_confirm_done())
+			end,
+		},
+
+		-- cmp sources plugins
+		{
+			"saadparwaiz1/cmp_luasnip",
+			"hrsh7th/cmp-nvim-lua",
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-cmdline",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-buffer",
+		},
+	},
+
 	config = function()
 		local cmp = require("cmp")
 		local opts = require("nvchad.configs.cmp")
