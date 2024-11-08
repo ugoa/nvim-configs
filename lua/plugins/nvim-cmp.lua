c35bbf1da3be6e5e8152d66d0fb51fccdd98102d
Dawei Hu <hoodavy@gmail.com>
2024-11-08 22:27:03 +0800

    migrate cmp plugin

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -13,7 +13,29 @@ return {
 			config = function(_, opts)
 				require("luasnip").config.set_config(opts)
 				require("luasnip").filetype_extend("htmldjango", { "html" })
-				require("nvchad.configs.luasnip")
+
+				-- vscode format
+				require("luasnip.loaders.from_vscode").lazy_load({ exclude = vim.g.vscode_snippets_exclude or {} })
+				require("luasnip.loaders.from_vscode").lazy_load({ paths = vim.g.vscode_snippets_path or "" })
+
+				-- snipmate format
+				require("luasnip.loaders.from_snipmate").load()
+				require("luasnip.loaders.from_snipmate").lazy_load({ paths = vim.g.snipmate_snippets_path or "" })
+
+				-- lua format
+				require("luasnip.loaders.from_lua").load()
+				require("luasnip.loaders.from_lua").lazy_load({ paths = vim.g.lua_snippets_path or "" })
+
+				vim.api.nvim_create_autocmd("InsertLeave", {
+					callback = function()
+						if
+							require("luasnip").session.current_nodes[vim.api.nvim_get_current_buf()]
+							and not require("luasnip").session.jump_active
+						then
+							require("luasnip").unlink_current()
+						end
+					end,
+				})
 			end,
 		},
 
@@ -46,7 +68,57 @@ return {
 
 	config = function()
 		local cmp = require("cmp")
-		local opts = require("nvchad.configs.cmp")
+		local opts = {
+			completion = { completeopt = "menu,menuone" },
+
+			snippet = {
+				expand = function(args)
+					require("luasnip").lsp_expand(args.body)
+				end,
+			},
+
+			mapping = {
+				["<C-p>"] = cmp.mapping.select_prev_item(),
+				["<C-n>"] = cmp.mapping.select_next_item(),
+				["<C-d>"] = cmp.mapping.scroll_docs(-4),
+				["<C-f>"] = cmp.mapping.scroll_docs(4),
+				["<C-Space>"] = cmp.mapping.complete(),
+				["<C-e>"] = cmp.mapping.close(),
+
+				["<CR>"] = cmp.mapping.confirm({
+					behavior = cmp.ConfirmBehavior.Insert,
+					select = true,
+				}),
+
+				["<Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_next_item()
+					elseif require("luasnip").expand_or_jumpable() then
+						require("luasnip").expand_or_jump()
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+
+				["<S-Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_prev_item()
+					elseif require("luasnip").jumpable(-1) then
+						require("luasnip").jump(-1)
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+			},
+
+			sources = {
+				{ name = "nvim_lsp" },
+				{ name = "luasnip" },
+				{ name = "buffer" },
+				{ name = "nvim_lua" },
+				{ name = "path" },
+			},
+		}
 		cmp.setup(opts)
 
 		cmp.setup.cmdline("/", {
