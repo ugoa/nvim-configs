e3fa23d6747928b0230298a08dde4f3d5f0eb2fd
Dawei Hu <hoodavy@gmail.com>
2024-11-09 16:52:42 +0800

    minor fix

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -51,6 +51,9 @@ return {
 				["vim.lsp.util.convert_input_to_markdown_lines"] = true,
 				["vim.lsp.util.stylize_markdown"] = true,
 			},
+			signature = {
+				enabled = false,
+			},
 		},
 		cmdline = {
 			format = {
