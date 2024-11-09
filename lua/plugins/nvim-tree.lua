da7761560866fe29a0cff5e46b97a3d5111f5b1f
Dawei Hu <hoodavy@gmail.com>
2024-11-09 16:29:25 +0800

    update nvimtree to 1.8

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,48 +1,19 @@
-local function open_or_expand()
-	local lib = require("nvim-tree.lib")
-	local node = lib.get_node_at_cursor()
-	if node.name == ".." then
-		return
-	elseif node.nodes then
-		lib.expand_or_collapse(node, false)
-	else
-		local path = node.absolute_path
-		if node.link_to and not node.nodes then
-			path = node.link_to
-		end
-		require("nvim-tree.actions").node.open_file.fn("edit", path)
-	end
-end
-
-local function my_on_attach(bufnr)
-	-- vim.cmd([[ :hi NvimTreeCursorLine guifg=cterm=bold gui=bold guibg=#4d4f68 ]])
-
+local function on_attach(bufnr)
 	local api = require("nvim-tree.api")
 
-	api.node.open.edit = open_or_expand
-
-	local function opts(desc)
-		return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
-	end
 	-- default mappings
 	api.config.mappings.default_on_attach(bufnr)
 
-	-- DISABLE
-	vim.keymap.set("n", "<C-]>", "<nop>", opts("Disabled"))
-	vim.keymap.set("n", "-", "<nop>", opts("Disabled"))
-	vim.keymap.set("n", "<2-RightMouse>", "<nop>", opts("Disabled"))
-	vim.keymap.set("n", "g?", "<nop>", opts("Disabled"))
-	vim.keymap.set("n", "H", "<nop>", opts("Disabled"))
-	vim.keymap.set("n", "U", "<nop>", opts("Disabled"))
-	vim.keymap.set("n", "I", "<nop>", opts("Disabled"))
-	vim.keymap.set("n", "C", "<nop>", opts("Disabled"))
-	vim.keymap.set("n", "M", "<nop>", opts("Disabled"))
-	vim.keymap.set("n", "B", "<nop>", opts("Disabled"))
-	vim.keymap.set("n", "f", "<nop>", opts("Disabled"))
-	vim.keymap.set("n", "F", "<nop>", opts("Disabled"))
-
+	local function opts(desc)
+		return {
+			desc = "nvim-tree: " .. desc,
+			buffer = bufnr,
+			noremap = true,
+			silent = true,
+			nowait = true,
+		}
+	end
 	-- custom mappings
-	vim.keymap.set("n", "?", api.tree.toggle_help, opts("Help"))
 	vim.keymap.set("n", "t", api.tree.toggle_enable_filters, opts("Toggle Filters"))
 	vim.keymap.set("n", "T", api.tree.change_root_to_node, opts("Set current node as root dir"))
 
@@ -58,13 +29,14 @@ end
 return {
 	"nvim-tree/nvim-tree.lua",
 	cmd = { "NvimTreeToggle", "NvimTreeFocus" },
+	tag = "v1.8.0",
 
 	-- https://github.com/nvim-tree/nvim-tree.lua/blob/master/lua/nvim-tree.lua#L237
 	opts = function()
 		dofile(vim.g.base46_cache .. "nvimtree")
 
 		return {
-			on_attach = my_on_attach,
+			on_attach = on_attach,
 			reload_on_bufenter = true,
 			filters = {
 				enable = true,
