f05bacc0cd0ab07e6b71d9c47bdb0f3ab574891a
Dawei Hu <hoodavy@gmail.com>
2024-11-13 01:22:49 +0800

    add more maps

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -82,6 +82,7 @@ return {
 				["<C-f>"] = cmp.mapping.scroll_docs(4),
 				["<C-k>"] = cmp.mapping.complete(),
 				["<C-e>"] = cmp.mapping.close(),
+				["<Esc>"] = cmp.mapping.close(), -- for cmd cancel
 
 				["<CR>"] = cmp.mapping.confirm({
 					behavior = cmp.ConfirmBehavior.Insert,
