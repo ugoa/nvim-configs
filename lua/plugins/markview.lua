f795296a387d38772690ae6f5b98337702f791d5
Dawei Hu <dawei.hu@gxs.com.sg>
2025-02-20 15:35:53 +0800

    chore: [main] fix config of markview

--- lua/plugins/markview.lua
+++ lua/plugins/markview.lua
@@ -2,13 +2,12 @@ return {
 	"OXY2DEV/markview.nvim",
 	tag = "v25.3.0",
 	ft = "markdown",
-	lazy = false,
 
 	dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
 
 	opts = {
-		initial_state = false,
 		preview = {
+			enable = false,
 			icon_provider = "devicons",
 		},
 	},
