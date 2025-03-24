20574f6a6af1a1d3c2f8ed9236a04e9fc950d774
Dawei Hu <hoodavy@gmail.com>
2025-03-24 23:02:45 +0800

    relocate

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -18,24 +18,14 @@ return {
 	},
 	-- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
 	build = "make",
-	-- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
 	dependencies = {
 		"nvim-treesitter/nvim-treesitter",
 		"stevearc/dressing.nvim",
 		"nvim-lua/plenary.nvim",
 		"MunifTanjim/nui.nvim",
-		--- The below dependencies are optional,
-		"nvim-telescope/telescope.nvim", -- for file_selector provider telescope
-		"hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
-		"nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-		{
-			-- Make sure to set this up properly if you have lazy=true
-			"MeanderingProgrammer/render-markdown.nvim",
-			dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
-			opts = {
-				file_types = { "markdown", "Avante" },
-			},
-			ft = { "markdown", "Avante" },
-		},
+		"nvim-telescope/telescope.nvim",
+		"hrsh7th/nvim-cmp",
+		"nvim-tree/nvim-web-devicons",
+		"MeanderingProgrammer/render-markdown.nvim",
 	},
 }
---
+++ lua/plugins/render-markdown.lua
@@ -0,0 +1,9 @@
+return {
+	-- Make sure to set this up properly if you have lazy=true
+	"MeanderingProgrammer/render-markdown.nvim",
+	dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
+	opts = {
+		file_types = { "markdown", "Avante" },
+	},
+	ft = { "markdown", "Avante" },
+}
