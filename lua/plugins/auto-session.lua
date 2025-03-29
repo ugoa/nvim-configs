65134bcc84121504659fa07eee00e0b13cc1c575
Dawei Hu <hoodavy@gmail.com>
2025-03-30 00:03:59 +0800

    reuse auto-session

--- lua/plugins/auto-session.lua
+++ lua/plugins/auto-session.lua
@@ -1,17 +1,8 @@
-local function openTree()
-	require("nvim-tree").change_dir(vim.fn.getcwd())
-	local api = require("nvim-tree.api")
-	api.tree.open()
-	api.node.open.edit()
-end
-
 return {
 	"rmagatti/auto-session",
-	-- lazy = false,
-	enabled = false,
+	lazy = false,
 	opts = {
+		suppressed_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
 		log_level = "error",
-		post_restore_cmds = { openTree },
-		cwd_change_handling = nil,
 	},
 }
