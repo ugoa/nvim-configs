ed7cd7dd8d348e1dac6aa9ca3361e1e4a71b8f15
Dawei Hu <dawei.hu@gxs.com.sg>
2025-03-29 22:53:59 +0800

    chore: [main] format with djlint

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -11,11 +11,24 @@ return {
 			javascriptreact = { "prettier" },
 			json = { "prettier" },
 			-- vue = { "prettier" },
-			-- html = { "djlint" },
+			html = { "djlint" },
+			htmldjango = { "djlint" },
 		},
 		format_on_save = { -- These options will be passed to conform.format()
-			timeout_ms = 4000, -- increase timeout to 2s since black take longer time to finish.
+			timeout_ms = 10000, -- increase timeout to 2s since black take longer time to finish.
 			lsp_fallback = true,
 		},
 	},
+
+	config = function(_, opts)
+		require("conform").setup(opts)
+
+		require("conform").formatters.djlint = function(bufnr)
+			return {
+				command = "djlint",
+				args = { "--reformat", "-" },
+				cwd = require("conform.util").root_file({ "djlint.toml" }),
+			}
+		end
+	end,
 }
