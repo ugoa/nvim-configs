d9579473de0535cd6e5dcbb58fc72a3037284e2f
Dawei Hu <hoodavy@gmail.com>
2025-03-31 13:56:49 +0800

    venv verylazy

--- lua/plugins/venv-selector.lua
+++ lua/plugins/venv-selector.lua
@@ -5,7 +5,6 @@ return {
 		"neovim/nvim-lspconfig",
 		{ "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" } },
 	},
-	lazy = false,
 	branch = "regexp",
 	keys = {
 		{ "<leader>jv", "<cmd>VenvSelect<cr>" },
