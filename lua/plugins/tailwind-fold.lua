efe8067b27e44cbb5d6066d6092a9d30121c0ef5
Dawei Hu <dawei.hu@gxs.com.sg>
2025-04-02 11:00:46 +0800

    chore: [main] disable tw

--- lua/plugins/tailwind-fold.lua
+++ lua/plugins/tailwind-fold.lua
@@ -1,5 +1,6 @@
 return {
 	"razak17/tailwind-fold.nvim",
+	enabled = false,
 	opts = {},
 	dependencies = { "nvim-treesitter/nvim-treesitter" },
 	ft = { "html", "vue", "jsx", "tsx", "htmldjango" },
