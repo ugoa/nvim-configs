8be2e7cafb8e3086673893b3857ced760e63a532
Dawei Hu <dawei.hu@gxs.com.sg>
2025-07-21 19:28:13 +0800

    chore: [work] toggle minifiles

--- lua/plugins/mini-files.lua
+++ lua/plugins/mini-files.lua
@@ -19,10 +19,12 @@ return {
 		{
 			"-",
 			function()
-				local buf_name = vim.api.nvim_buf_get_name(0)
-				local path = vim.fn.filereadable(buf_name) == 1 and buf_name or vim.fn.getcwd()
-				MiniFiles.open(path)
-				MiniFiles.reveal_cwd()
+				if not MiniFiles.close() then
+					local buf_name = vim.api.nvim_buf_get_name(0)
+					local path = vim.fn.filereadable(buf_name) == 1 and buf_name or vim.fn.getcwd()
+					MiniFiles.open(path)
+					MiniFiles.reveal_cwd()
+				end
 			end,
 			desc = "Open Mini Files",
 		},
