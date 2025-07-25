4aa0082ccc323b3b3302476acee2541dd4f291f5
Dawei Hu <hoodavy@gmail.com>
2025-07-26 02:03:08 +0800

    config lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -7,6 +7,15 @@ return {
 			options = {
 				theme = "catppuccin-mocha",
 			},
+
+			sections = {
+				lualine_c = {
+					{
+						"filename",
+						path = 1, -- Displays path relative to cmd
+					},
+				},
+			},
 		})
 	end,
 }
