970ad6e41696ffd734a8f0d0f2dcfde52de2eb25
Dawei Hu <dawei.hu@gxs.com.sg>
2025-07-25 11:30:23 +0800

    fix nvchad

--- lua/plugins/a--ui.lua
+++ lua/plugins/a--ui.lua
@@ -1,6 +1,7 @@
 return {
 	{
 		"nvchad/ui",
+		lazy = false,
 		config = function()
 			require("nvchad")
 		end,
@@ -8,6 +9,7 @@ return {
 
 	{
 		"nvchad/base46",
+		lazy = false,
 		build = function()
 			require("base46").load_all_highlights()
 		end,
