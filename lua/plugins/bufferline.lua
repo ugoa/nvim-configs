a56717e297faca7cbef00965d355f6bc6e1895d8
Dawei Hu <hoodavy@gmail.com>
2025-07-26 02:00:17 +0800

    lualine and bufline

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -5,8 +5,7 @@ return {
 	dependencies = "nvim-tree/nvim-web-devicons",
 	config = function(_, opts)
 		require("bufferline").setup({
-			options = {},
-			highlights = require("catppuccin.groups.integrations.bufferline").get({}),
+			highlights = require("catppuccin.groups.integrations.bufferline").get(),
 		})
 	end,
 }
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,6 +1,12 @@
 return {
 	"nvim-lualine/lualine.nvim",
 	lazy = false,
-	opts = {},
 	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		require("lualine").setup({
+			options = {
+				theme = "catppuccin-mocha",
+			},
+		})
+	end,
 }
