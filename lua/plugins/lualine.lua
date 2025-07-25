5cb83d1d2ae0f5e60b939b3b16f3dc38b4f35e35
Dawei Hu <hoodavy@gmail.com>
2025-07-26 02:17:03 +0800

    show lsp in lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,3 +1,16 @@
+local lsp = function()
+	if rawget(vim, "lsp") then
+		for _, client in ipairs(vim.lsp.get_clients()) do
+			local stbufnr = vim.api.nvim_win_get_buf(vim.g.statusline_winid or 0)
+			if client.attached_buffers[stbufnr] then
+				return (vim.o.columns > 100 and "  LSP: " .. client.name .. " ") or "   LSP "
+			end
+		end
+	end
+
+	return ""
+end
+
 return {
 	"nvim-lualine/lualine.nvim",
 	lazy = false,
@@ -14,6 +27,11 @@ return {
 						"filename",
 						path = 1, -- Displays path relative to cmd
 					},
+				},
+				lualine_x = {
+					"encoding",
+					"filetype",
+					lsp,
 				},
 			},
 		})
