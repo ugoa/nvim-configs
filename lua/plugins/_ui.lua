2a2aac8680331ebeeb24c2d017e067d6c9380119
Dawei Hu <hoodavy@gmail.com>
2025-07-26 10:36:34 +0800

    reorg

---
+++ lua/plugins/_ui.lua
@@ -0,0 +1,76 @@
+local lsp = function()
+	if rawget(vim, "lsp") then
+		for _, client in ipairs(vim.lsp.get_clients()) do
+			local stbufnr = vim.api.nvim_win_get_buf(vim.g.statusline_winid or 0)
+			if client.attached_buffers[stbufnr] then
+				return (vim.o.columns > 100 and "  LSP: " .. client.name .. " ") or "   LSP "
+			end
+		end
+	end
+
+	return ""
+end
+
+return {
+	{
+		"catppuccin/nvim",
+		lazy = false,
+		name = "catppuccin",
+		priority = 1000,
+		opts = {
+			flavour = "mocha",
+		},
+	},
+
+	{
+		"akinsho/bufferline.nvim",
+		lazy = false,
+		version = "*",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		after = "catppuccin",
+		config = function()
+			require("bufferline").setup({
+				highlights = require("catppuccin.groups.integrations.bufferline").get(),
+			})
+		end,
+	},
+
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		after = "catppuccin",
+		event = "VeryLazy",
+		init = function()
+			vim.g.lualine_laststatus = vim.o.laststatus
+			if vim.fn.argc(-1) > 0 then
+				-- set an empty statusline till lualine loads
+				vim.o.statusline = " "
+			else
+				-- hide the statusline on the starter page
+				vim.o.laststatus = 0
+			end
+		end,
+		opts = {
+			options = {
+				theme = "catppuccin",
+			},
+			sections = {
+				lualine_c = {
+					{
+						"filename",
+						path = 1, -- Displays path relative to cmd
+					},
+				},
+				lualine_x = {
+					"encoding",
+					"filetype",
+					lsp,
+				},
+			},
+		},
+	},
+
+	{ "nvim-tree/nvim-web-devicons", opts = {} },
+
+	"nvim-lua/plenary.nvim",
+}
