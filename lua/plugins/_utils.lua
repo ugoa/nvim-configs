28f9381430bb1418f656178b5fddb124a99349f2
Dawei Hu <dawei.hu@gxs.com.sg>
2025-07-28 17:36:49 +0800

    chore: [work2] update bookmarks

--- lua/plugins/_utils.lua
+++ lua/plugins/_utils.lua
@@ -169,12 +169,6 @@ return {
 
 				-- sign_priority = 8,  --set bookmark sign priority to cover other sign
 				save_file = vim.fn.expand("$HOME/.bookmarks"), -- bookmarks save file path
-				keywords = {
-					["@t"] = "☑️ ", -- mark annotation startswith @t ,signs this icon as `Todo`
-					["@w"] = "⚠️ ", -- mark annotation startswith @w ,signs this icon as `Warn`
-					["@f"] = "⛏ ", -- mark annotation startswith @f ,signs this icon as `Fix`
-					["@n"] = " ", -- mark annotation startswith @n ,signs this icon as `Note`
-				},
 				on_attach = function(bufnr)
 					local bm = require("bookmarks")
 					local map = vim.keymap.set
