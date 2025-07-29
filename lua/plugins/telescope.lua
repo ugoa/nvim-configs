5547ed6a15688fa43040a57f9b299cc408cab4a6
Dawei Hu <hoodavy@gmail.com>
2025-07-29 23:25:38 +0800

    tab to focus on/off tele preview

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,3 +1,17 @@
+local function focus_preview(prompt_bufnr)
+	local action_state = require("telescope.actions.state")
+	local picker = action_state.get_current_picker(prompt_bufnr)
+	local prompt_win = picker.prompt_win
+	local previewer = picker.previewer
+	local bufnr = previewer.state.bufnr or previewer.state.termopen_bufnr
+	local winid = previewer.state.winid or vim.fn.win_findbuf(bufnr)[1]
+	vim.keymap.set("n", "<Tab>", function()
+		vim.cmd(string.format("noautocmd lua vim.api.nvim_set_current_win(%s)", prompt_win))
+	end, { buffer = bufnr })
+	vim.cmd(string.format("noautocmd lua vim.api.nvim_set_current_win(%s)", winid))
+	-- api.nvim_set_current_win(winid)
+end
+
 return {
 	"nvim-telescope/telescope.nvim",
 	dependencies = { "nvim-treesitter/nvim-treesitter" },
@@ -46,7 +60,7 @@ return {
 				height = 0.80,
 			},
 			mappings = {
-				i = { ["<Esc>"] = require("telescope.actions").close },
+				i = { ["<Esc>"] = require("telescope.actions").close, ["<Tab>"] = focus_preview },
 			},
 		},
 
