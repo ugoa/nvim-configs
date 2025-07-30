eaa9c66cd5689b8a2c65ba250fdd6723970c851c
Dawei Hu <hoodavy@gmail.com>
2025-07-30 21:18:09 +0800

    clean treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,48 +1,45 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
 	event = { "BufReadPost", "BufNewFile" },
-	cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
 	build = ":TSUpdate",
-	dependencies = {
-		-- NOTE: additional parser
-		{ "nushell/tree-sitter-nu", build = ":TSUpdate nu" },
-	},
-	opts = {
-		ensure_installed = {
-			"rust",
-			"python",
-			"yaml",
-			"bash",
-			"regex",
-			"markdown",
-			"markdown_inline",
-			"vue",
-			"javascript", -- jsx included
-			"typescript",
-			"tsx",
-			"html",
-			"css",
-			"scss",
-			"terraform",
-			"just",
-			"htmldjango",
-			"json",
-			"sql",
-			"java",
-			"scala",
-			"nu",
-			"toml",
-			"make",
-		},
-		highlight = {
-			enable = true,
-			use_languagetree = true,
-		},
-		indent = { enable = true },
-	},
-	-- IMPORTANT to make TS highlight to work.
-	-- https://github.com/MeanderingProgrammer/render-markdown.nvim/issues/249#issuecomment-2519081718
-	config = function(_, opts)
+	config = function()
+		local opts = {
+			ensure_installed = {
+				"nu",
+				"rust",
+				"python",
+				"yaml",
+				"bash",
+				"regex",
+				"markdown",
+				"markdown_inline",
+				"vue",
+				"javascript", -- jsx included
+				"typescript",
+				"tsx",
+				"html",
+				"css",
+				"scss",
+				"terraform",
+				"just",
+				"htmldjango",
+				"json",
+				"sql",
+				"java",
+				"scala",
+				"nu",
+				"toml",
+				"make",
+			},
+			highlight = {
+				enable = true,
+				use_languagetree = true,
+			},
+			indent = { enable = true },
+		}
+
+		-- IMPORTANT to make TS highlight to work.
+		-- https://github.com/MeanderingProgrammer/render-markdown.nvim/issues/249#issuecomment-2519081718
 		require("nvim-treesitter.configs").setup(opts)
 	end,
 }
