63624c592b5109848802641057d20faa76cf9819
Dawei Hu <hoodavy@gmail.com>
2025-07-31 21:32:58 +0800

    fix cc

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -6,7 +6,31 @@ return {
 			"<cmd>CodeCompanionChat Toggle<CR>",
 		},
 	},
-	opts = {},
+	opts = {
+		strategies = {
+			chat = {
+				adapter = "gemini",
+			},
+			inline = {
+				adapter = "gemini",
+			},
+			cmd = {
+				adapter = "gemini",
+			},
+		},
+		adapters = {
+			gemini = function()
+				-- API_KEY default from external env GEMINI_API_KEY
+				return require("codecompanion.adapters").extend("gemini", {
+					schema = {
+						model = {
+							default = "gemini-2.0-flash",
+						},
+					},
+				})
+			end,
+		},
+	},
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		"nvim-treesitter/nvim-treesitter",
