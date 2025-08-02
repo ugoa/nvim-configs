9b58652d1ac8f3a9243316cf09d0fc678b6f438e
Dawei Hu <hoodavy@gmail.com>
2025-08-02 23:56:32 +0800

    move dressing

--- lua/plugins/2--utils.lua
+++ lua/plugins/2--utils.lua
@@ -1,21 +1,7 @@
 return {
   { "nvim-tree/nvim-web-devicons", opts = {} },
 
-  "nvim-lua/plenary.nvim",
-
-  {
-    "stevearc/dressing.nvim",
-    opts = {
-      input = {
-        enabled = true,
-      },
-      select = {
-        enabled = true,
-        backend = { "telescope", "builtin" },
-        telescope = require("telescope.themes").get_cursor(),
-      },
-    },
-  },
+  { "nvim-lua/plenary.nvim" },
 
   { "norcalli/nvim-colorizer.lua", cmd = "ColorizerToggle", opts = {} },
 }
---
+++ lua/plugins/dressing.lua
@@ -0,0 +1,13 @@
+return {
+  "stevearc/dressing.nvim",
+  opts = {
+    input = {
+      enabled = true,
+    },
+    select = {
+      enabled = true,
+      backend = { "telescope", "builtin" },
+      telescope = require("telescope.themes").get_cursor(),
+    },
+  },
+}
