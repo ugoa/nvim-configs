77ee9de4d9248657e4235c60705291c4729d257d
Dawei Hu <hoodavy@gmail.com>
2025-08-02 18:04:51 +0800

    update ft

--- lua/plugins/jinja.lua
+++ lua/plugins/jinja.lua
@@ -5,7 +5,7 @@ return {
 		local autocmd = vim.api.nvim_create_autocmd
 
 		autocmd({ "FileType" }, {
-			pattern = { "html", "sql" },
+			pattern = { "html", "sql", "yaml", "yml", "python" },
 			callback = function()
 				vim.cmd([[ call jinja#AdjustFiletype() ]])
 			end,
