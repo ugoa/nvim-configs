dfc1655f379652e6d93ce662b1004379e86057d3
Dawei Hu <hoodavy@gmail.com>
2025-08-03 16:30:40 +0800

    claanup files

--- lua/plugins/2--utils.lua
+++ lua/plugins/2--utils.lua
@@ -4,4 +4,8 @@ return {
   { "nvim-lua/plenary.nvim" },
 
   { "norcalli/nvim-colorizer.lua", cmd = "ColorizerToggle", opts = {} },
+
+  { "sphamba/smear-cursor.nvim", cmd = { "SmearCursorToggle" }, opts = {} },
+
+  { "stevearc/quicker.nvim", event = "FileType qf", opts = {} },
 }
