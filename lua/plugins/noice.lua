b7e1d80d366e9105de609f0bca6a0f521634d00f
Dawei Hu <hoodavy@gmail.com>
2025-08-06 00:25:36 +0800

    fix notify

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -6,8 +6,9 @@ return {
     {
       "rcarriga/nvim-notify",
       opts = {
-        background_colour = "#000000",
-        timeout = 5000,
+        timeou = 5000,
+        render = "wrapped-compact",
+        stages = "fade",
       },
     },
 
