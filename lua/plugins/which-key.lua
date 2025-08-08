18d9a9f353b7c1c551419baac93314863c451995
Dawei Hu <hoodavy@gmail.com>
2025-08-08 22:24:21 +0800

    update wk

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,10 +1,6 @@
 return {
   "folke/which-key.nvim",
   keys = { "<leader>", "<c-w>", '"', "'", "`", "c", "v", "g" },
-  lazy = false,
-  config = function(_, opts)
-    vim.opt.timeout = true
-    vim.opt.timeoutlen = 0
-    require("which-key").setup(opts)
-  end,
+  event = "VeryLazy",
+  config = true,
 }
