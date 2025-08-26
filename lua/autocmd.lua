0b93bf4830bf89e44a5f3cf0aac505f11e54bd74
Dawei Hu <dawei.hu@gxs.com.sg>
2025-08-26 11:09:28 +0800

    chore: [master] update

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -100,13 +100,13 @@ autocmd("BufWritePost", {
 })
 
 -- Open nvim-tree when startup, then move focus to buffer without closing the tree
-autocmd("VimEnter", {
-  callback = function()
-    require("nvim-tree.api").tree.open()
-    if vim.fn.bufname():match("NvimTree_") then
-      vim.cmd.wincmd("p")
-    else
-      vim.cmd("NvimTreeFindFile")
-    end
-  end,
-})
+-- autocmd("VimEnter", {
+--   callback = function()
+--     require("nvim-tree.api").tree.open()
+--     if vim.fn.bufname():match("NvimTree_") then
+--       vim.cmd.wincmd("p")
+--     else
+--       vim.cmd("NvimTreeFindFile")
+--     end
+--   end,
+-- })
