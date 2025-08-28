e8e24e91d7ae01204f86d07772bfe78a44166bb9
Dawei Hu <dawei.hu@gxs.com.sg>
2025-08-28 23:13:48 +0800

    chore: [master] add scala lsp

--- lua/plugins/scala-lsp.lua
+++ lua/plugins/scala-lsp.lua
@@ -1,6 +1,7 @@
 local map = vim.keymap.set
 return {
   "scalameta/nvim-metals",
+  enabled = false,
   ft = { "scala", "sbt", "java" },
   opts = function()
     local metals_config = require("metals").bare_config()
@@ -26,8 +27,6 @@ return {
     metals_config.capabilities = require("cmp_nvim_lsp").default_capabilities()
 
     metals_config.on_attach = function(client, bufnr)
-      require("metals").setup_dap()
-
       -- LSP mappings
       map("n", "gD", vim.lsp.buf.definition)
       map("n", "K", vim.lsp.buf.hover)
@@ -58,22 +57,6 @@ return {
       map("n", "[c", function() vim.diagnostic.goto_prev({ wrap = false }) end)
 
       map("n", "]c", function() vim.diagnostic.goto_next({ wrap = false }) end)
-
-      -- Example mappings for usage with nvim-dap. If you don't use that, you can
-      -- skip these
-      map("n", "<leader>dc", function() require("dap").continue() end)
-
-      map("n", "<leader>dr", function() require("dap").repl.toggle() end)
-
-      map("n", "<leader>dK", function() require("dap.ui.widgets").hover() end)
-
-      map("n", "<leader>dt", function() require("dap").toggle_breakpoint() end)
-
-      map("n", "<leader>dso", function() require("dap").step_over() end)
-
-      map("n", "<leader>dsi", function() require("dap").step_into() end)
-
-      map("n", "<leader>dl", function() require("dap").run_last() end)
     end
 
     return metals_config
