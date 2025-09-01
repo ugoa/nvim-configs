9aa4d444e3eaf125b90d5594e4340fc530c87922
Dawei Hu <dawei.hu@gxs.com.sg>
2025-09-01 15:25:39 +0800

    chore: [master] cleanup

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,10 +1,5 @@
 return {
   "folke/which-key.nvim",
-  keys = { "<leader>", "<c-w>", '"', "'", "`", "c", "v", "g" },
   event = "VeryLazy",
-  opts = {
-    disable = {
-      -- bt = { "terminal" },
-    },
-  },
+  config = true,
 }
